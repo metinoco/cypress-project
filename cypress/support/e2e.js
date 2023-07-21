cbdbcb7bbaef4e13b84431986254f9ad81a285cf
monshy_tinoco <monshy.tinoco@rocketfy.co>
2023-07-21 15:21:43 -0500

    Add uncaught:exception on e2e

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,9 @@
 import '@shelex/cypress-allure-plugin';
 import './commands'
+
+//Salta los errores que se presenten en el test
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
