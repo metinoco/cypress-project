719892f2eabb3083112e41c8cc70ad6bdce656a6
monshy_tinoco <djmonshy_tinoco@hotmail.com>
2023-07-25 09:42:56 -0500

    fix Debuging errors, flakiness and retries

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  //realiza el # de intentos si la prueba falla
+  retries: 1,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
+import { LoginMethods } from "./Pages/Login/login.methods"
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    const usuario = 'random01'
+    const contrasena = 'random01'
+    cy.visit('https://demoblaze.com/')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethods.login (usuario, contrasena)
+    cy.get('a#nameofuser').should('contain.text', usuario)
+    
   })
 })
