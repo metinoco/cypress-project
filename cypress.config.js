f362ecef13e3a13dabe0d0c976402bb997f09db5
monshy_tinoco <djmonshy_tinoco@hotmail.com>
2023-08-06 10:16:11 -0500

    Agregar logger

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
   //realiza el # de intentos si la prueba falla
   retries: 1,
+  //desactivar el reporte de video
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,17 +1,37 @@
 /// <reference types="Cypress" />
 import { CartMethods } from "./Pages/Cart/cart.methods"
 import { LoginMethods } from "./Pages/Login/login.methods"
+import { Logger } from "./util/logger"
 
 describe('template spec', () => {
   it('passes', () => {
     const usuario = 'random01'
     const contrasena = 'random01'
+
+    Logger.stepNumber(1)
+    Logger.step('Navigate to Demoblaze')
     cy.visit('https://demoblaze.com/')
-    cy.wait(20000);
-    // cy.get('a[data-target="#logInModal"]').click()
-    // LoginMethods.login (usuario, contrasena)
-    // cy.get('a#nameofuser').should('contain.text', usuario)
-    CartMethods.clickOnDeleteLink('Nokia lumia 1520')
-    cy.wait(20000);
+
+    Logger.stepNumber(2)
+    Logger.step('Click on Login link')
+    cy.get('a[data-target="#logInModal"]').click()
+
+
+    Logger.stepNumber(3)
+    Logger.step(`Login with this credentials: "${usuario}/${contrasena}"`)
+    LoginMethods.login(usuario, contrasena)
+    Logger.verification(`The Home should show "Welcome ${usuario}" text`)
+    cy.get('a#nameofuser').should('contain.text', usuario)
+    
+    
+    
+    
+    
+    // cy.wait(20000);
+    
+    // // LoginMethods.login (usuario, contrasena)
+    
+    // CartMethods.clickOnDeleteLink('Nokia lumia 1520')
+    // cy.wait(20000);
   })
 })
