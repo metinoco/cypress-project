36db1109ebfa179c479498b9c50f3457db89df88
monshy_tinoco <djmonshy_tinoco@hotmail.com>
2023-08-06 08:11:40 -0500

    Creación de la página Place Order

---
+++ cypress/e2e/Pages/Place-Order/place-order.data.js
@@ -0,0 +1,12 @@
+export class PlaceOrderData {
+    static get testDate(){
+        return {
+            name: 'Test name',
+            country: 'United States',
+            city: 'Boston',
+            creditCard:'4391993331439526',
+            month: 12,
+            year: 2024
+        }
+    }
+}
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="Cypress" />
+import { CartMethods } from "./Pages/Cart/cart.methods"
 import { LoginMethods } from "./Pages/Login/login.methods"
 
 describe('template spec', () => {
@@ -6,9 +7,11 @@ describe('template spec', () => {
     const usuario = 'random01'
     const contrasena = 'random01'
     cy.visit('https://demoblaze.com/')
-    cy.get('a[data-target="#logInModal"]').click()
-    LoginMethods.login (usuario, contrasena)
-    cy.get('a#nameofuser').should('contain.text', usuario)
-    
+    cy.wait(20000);
+    // cy.get('a[data-target="#logInModal"]').click()
+    // LoginMethods.login (usuario, contrasena)
+    // cy.get('a#nameofuser').should('contain.text', usuario)
+    CartMethods.clickOnDeleteLink('Nokia lumia 1520')
+    cy.wait(20000);
   })
 })
